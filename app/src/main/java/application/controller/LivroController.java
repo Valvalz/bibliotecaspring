39c3459781a85523c3e614e3377120b6d12e636f
Valvalz <bentogabryel048@gmail.com>
2023-05-24 17:54:20 +0000

    Salvar

--- app/src/main/java/application/controller/LivroController.java
+++ app/src/main/java/application/controller/LivroController.java
@@ -2,6 +2,8 @@ package application.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import application.model.Livro;
 import application.model.LivroRepository;
@@ -23,4 +25,12 @@ public class LivroController {
       return "/WEB-INF/livro/insert.jsp";
     }
     
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public String insert(@RequestParam("titulo") String titulo) {
+      Livro livro = new Livro();
+      livro.setTitulo(titulo);
+
+      LivroRepo.save(livro);
+      return "redirect:/livro/list";
+    }
 }
